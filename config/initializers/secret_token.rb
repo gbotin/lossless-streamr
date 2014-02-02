d446cc911b539922730a115e4c09c02a9396c3ed
Geoffrey BOTIN <geoffrey.botin@gmail.com>
2014-02-02 12:53:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LosslessStreamr::Application.config.secret_key_base = '6515c2a4f3168140e3df6ae4a2a1ac0b8eeed1a052ff1ed6399584c8a66203c1fa40eef6f7d8a77bfcd6606f6872468ca446d8db1d3c37a1fa6911d46de1a956'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LosslessStreamr::Application.config.session_store :cookie_store, key: '_lossless-streamr_session'
