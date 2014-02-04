323a6e4f2683f2bb0bb391fa8522d88050d42464
Geoffrey BOTIN <geoffrey.botin@gmail.com>
2014-02-04 13:53:14 +0100

    Heroku test

--- ngapp/Gruntfile.js
+++ ngapp/Gruntfile.js
@@ -62,9 +62,15 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
+      proxies: [
+        {
+          host: 'localhost',
+          port: 3000
+        }
+      ],
       livereload: {
         options: {
           open: true,
